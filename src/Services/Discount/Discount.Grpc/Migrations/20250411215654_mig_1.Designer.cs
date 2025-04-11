c9026caea1eae512c55e812e6a11367725bbcf9a
burakgunce <brk.gnc9@hotmail.com>
2025-04-12 01:44:22 +0300

    discount service updated and discount grpc layer containerized and orchestrated.

--- src/Services/Discount/Discount.Grpc/Migrations/20250411215654_mig_1.Designer.cs
+++ src/Services/Discount/Discount.Grpc/Migrations/20250411215654_mig_1.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Discount.Grpc.Migrations
 {
     [DbContext(typeof(DiscountContext))]
-    [Migration("20250411205620_mig_1")]
+    [Migration("20250411215654_mig_1")]
     partial class mig_1
     {
         /// <inheritdoc />
@@ -39,6 +39,22 @@ namespace Discount.Grpc.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Coupons");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            Amount = 150,
+                            Description = "IPhone Discount",
+                            ProductName = "IPhone X"
+                        },
+                        new
+                        {
+                            Id = 2,
+                            Amount = 100,
+                            Description = "Samsung Discount",
+                            ProductName = "Samsung 10"
+                        });
                 });
 #pragma warning restore 612, 618
         }
